7ca946448ec1ec3dc34873c862dd1dd9b711ada3
tharanamayu <tharana.maya@gmail.com>
2018-10-19 00:01:07 +0530

    updated all files

--- notes.js
+++ notes.js
@@ -1,26 +1,35 @@
-
 console.log('Starting notes.js');
 
 //console.log(module);
 const fs= require('fs');
 
-var fetchNotes =()=>{
-  try {
-    var notesString=fs.readFileSync('notes-data.json');
-    return JSON.parse(notesString);
-  } catch (e) {
-    return [];
-  }
-};
 var addNote =(title,body)=>{
 // console.log('Adding note',title,body);
-var notes =[];
-var note ={
+var notes =[]; //array
+var note ={   //json object
   title,
   body
 };
 
+var fetchNotes =()=>{
+  try {
+    var notesString=fs.readFileSync('notes-data.json'); //if it is not "notes-data.json" file this doesn't read
+    return JSON.parse(notesString);
+  } catch (e) {
+      return [];
+  }
+};
 
+var saveNotes=(notes)=>{
+
+};
+
+
+
+var duplicateNotes =notes.filter((note) =>{
+return note.title===title;
+
+});
 notes.push(note);
 fs.writeFileSync('notes-data.json',JSON.stringify(notes));
 
